bef7660eee3298255d980817f49280734e41e420
Saulo <scadete@gmail.com>
2017-10-01 06:24:48 -0300

    Fixes condition to send AUDIO attachments to api.ai

--- src/main/java/com/github/scadete/regula/messenger/handler/RegulaAttachmentMessageEventHandler.java
+++ src/main/java/com/github/scadete/regula/messenger/handler/RegulaAttachmentMessageEventHandler.java
@@ -47,7 +47,7 @@ public class RegulaAttachmentMessageEventHandler  extends RegulaEventHandler imp
             if (payload.isBinaryPayload()) {
                 payloadAsString = payload.asBinaryPayload().getUrl();
 
-                if (attachment.equals("AUDIO")) {
+                if (attachmentType.equals("AUDIO")) {
                     try {
                         InputStream voiceStream = new URL(payload.asBinaryPayload().getUrl()).openStream();
                         response = chatbot.audio(voiceStream, senderId);
