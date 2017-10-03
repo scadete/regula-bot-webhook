b3822806339cc8de2fe56efeb7683580df4d2036
Saulo <scadete@gmail.com>
2017-10-02 21:22:05 -0300

    Reducing log level

--- src/main/java/com/github/scadete/regula/RegulaApplication.java
+++ src/main/java/com/github/scadete/regula/RegulaApplication.java
@@ -1,13 +1,20 @@
 package com.github.scadete.regula;
 
+import com.github.scadete.regula.stt.SpeechToTextService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 public class RegulaApplication {
     private static final Logger logger = LoggerFactory.getLogger(RegulaApplication.class);
+    @Autowired
+    @Resource(name="googleSpeechToTextService")
+    SpeechToTextService sttService;
 
     public static void main(String[] args) {
         logger.debug("RegulaApplication");
